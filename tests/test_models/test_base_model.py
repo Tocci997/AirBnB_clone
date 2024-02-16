15c252b0b71791074e4a4aa77bd3a6c0c664e86d
Tocci997 <duet997@gmail.com>
2024-02-16 12:09:21 -0800

    AirBnB

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -36,36 +36,6 @@ class BaseModelTest(unittest.TestCase):
         except IOError:
             pass
 
-    def test_one_save(self):
-        bm = BaseModel()
-        sleep(0.05)
-        first_updated_at = bm.updated_at
-        bm.save()
-        self.assertLess(first_updated_at, bm.updated_at)
-
-    def test_two_saves(self):
-        bm = BaseModel()
-        sleep(0.05)
-        first_updated_at = bm.updated_at
-        bm.save()
-        second_updated_at = bm.updated_at
-        self.assertLess(first_updated_at, second_updated_at)
-        sleep(0.05)
-        bm.save()
-        self.assertLess(second_updated_at, bm.updated_at)
-
-    def test_save_with_arg(self):
-        bm = BaseModel()
-        with self.assertRaises(TypeError):
-            bm.save(None)
-
-    def test_save_updates_file(self):
-        bm = BaseModel()
-        bm.save()
-        bmid = "BaseModel." + bm.id
-        with open("file.json", "r") as f:
-            self.assertIn(bmid, f.read())
-
     def test_to_dict(self):
         eg_model = BaseModel()
         eg_model_dict = eg_model.to_dict()
